1c6fa3e3c0d88302c15bff5e2122d901ffdd75d7
Arfourie <aristafourie@gmail.com>
2023-09-10 22:05:47 +0200

    Create 2_Summarise kaiju table for SS summaries.R

---
+++ 2_Summarise kaiju table for SS summaries.R
@@ -0,0 +1,26 @@
+#August 2023
+#Arista Fourie
+#Summarise the taxonomic classification of the reads for each sample dataset, as produced by Kaiju. 
+#This script is based on phylum rank summaries and also filters for the specific phyla of interest to obtain the total number of reads assigned to each phylum 
+
+library(dplyr)
+kaiju_phyla <- read.table("All_MGs_kaiju_phylum.tsv", header = TRUE, sep = "\t", dec = ".") %>%   #Output from kaiju analysis table output on phylum level
+  filter(Kingdom=="Bacteria") %>% 
+  dplyr::rename(sample = file)
+MG_samples <- unique(kaiju_phyla$sample)
+
+#Run script for all sample files
+All_MGs_phyla <- data.frame()
+for (i in MG_samples){
+  name=i
+  Sample_phyla <- filter(kaiju_phyla,sample==i)
+  Total_reads <- sum(Sample_phyla$reads)
+  Sample_phyla$RA <- Sample_phyla$reads / Total_reads
+  Phyla_info <- filter(Sample_phyla, Phylum=="Proteobacteria" | Phylum=="Bacteroidetes" | 
+                            Phylum=="Chlamydiae")
+  All_MGs_phyla <- rbind(All_MGs_phyla,Phyla_info)
+}
+All_MGs_phyla_select <- All_MGs_phyla %>% 
+  select(sample,RA,Phylum)
+write.table(All_MGs_phyla_select,"Kaiju_phyla_summary.tsv", sep = "\t",
+            col.names = TRUE, row.names = FALSE, quote = FALSE)
