f75363ea0319028e6f3b1fe88f8144759d6b6fa6
Arfourie <aristafourie@gmail.com>
2024-04-30 13:06:44 +0200

    Update Identification of SSs3_Summarise all plants SS t-test.R

--- Identification of SSs3_Summarise all plants SS t-test.R
+++ Identification of SSs3_Summarise all plants SS t-test.R
@@ -18,7 +18,7 @@ SS_summaries_All <- data.frame()
 for (i in SS_summaries) {
   SS_per_plant <- read.table(i,header = TRUE, sep = "\t")
   plant <- sapply(strsplit(basename(i), "_SS"), `[`, 1)
-  SS_per_plant$plant <- rep(plant, times=length(SS_per_plant$med_TPM))
+  SS_per_plant$plant <- rep(plant, times=length(SS_per_plant$med_MRPM))
   SS_summaries_All <- rbind(SS_summaries_All,SS_per_plant) 
 }
 
@@ -31,7 +31,7 @@ soil <-c("SRR908290","SRR908291","SRR908279","SRR908281","SRR5195136","SRR519513
 Summary_ed <- mutate(SS_summaries_All, source=ifelse((sample %in% root),"Root",sample))
 Summary_ed <- mutate(Summary_ed, source=ifelse((sample %in% soil),"Soil",source))
 Summary_ed <- Summary_ed %>% 
-  select(-med_TPM) %>% 
+  select(-med_MRPM) %>% 
   filter(SS!= "T4SS.I")
 
 plant_types <- unique(Summary_ed$plant)
@@ -51,11 +51,11 @@ for (i in plant_types){
   summary_statsR <- Plant.indiv_ed %>% 
     filter(source == "Root") %>% 
     group_by(SS) %>% 
-    summarise(Root_meanTPM = mean(Adjusted_abund))
+    summarise(Root_meanMRPM = mean(Adjusted_abund))
   summary_statsS <- Plant.indiv_ed %>% 
     filter(source == "Soil") %>% 
     group_by(SS) %>% 
-    summarise(Soil_meanTPM = mean(Adjusted_abund))
+    summarise(Soil_meanMRPM = mean(Adjusted_abund))
   Output <- left_join(Output,summary_statsR, by="SS")
   Output <- left_join(Output,summary_statsS, by="SS")
   Plant.ttest.summary <- rbind(Plant.ttest.summary,Output)
@@ -69,8 +69,8 @@ for (i in plant_types){
   Logfold_dat <- filter(Summary_ed,plant == i)
   Logfold_dat <- Logfold_dat %>% 
     group_by(SS,source) %>% 
-    summarise(avg_TPM=mean(Adjusted_abund))
-  Logfold_dat_ed <- pivot_wider(Logfold_dat, names_from = source, values_from = avg_TPM)
+    summarise(avg_MRPM=mean(Adjusted_abund))
+  Logfold_dat_ed <- pivot_wider(Logfold_dat, names_from = source, values_from = avg_MRPM)
   Logfold_dat_ed$ratio <- Logfold_dat_ed$Root / Logfold_dat_ed$Soil
   Logfold_dat_ed$log2 <- log2(Logfold_dat_ed$ratio)
   Logfold_dat_ed$plant <- rep(i, times=length(Logfold_dat_ed$log2))
@@ -88,15 +88,15 @@ Combined_logFold$Plant_SS <- paste0(Combined_logFold$plant,"_",Combined_logFold$
 Combined_logFold <- left_join(Combined_logFold,Pval_info, by="Plant_SS")
 
 #Use the abundance of the SS in the rhizosphere for relative size of dots in the dotplot, where larger dots will indicate higher abundance
-TPM_select <- Combined_logFold
-TPM_select$TPM_abund <- ifelse((TPM_select$Root > TPM_select$Soil),TPM_select$Root,TPM_select$Soil)
+MRPM_select <- Combined_logFold
+MRPM_select$MRPM_abund <- ifelse((MRPM_select$Root > MRPM_select$Soil),MRPM_select$Root,MRPM_select$Soil)
 
 #Create final figure, including the logfold data, t-test p-values and abundance of SS in the rhizosphere
-Fig_labels <-  TPM_select$plant %>% 
+Fig_labels <-  MRPM_select$plant %>% 
     unique()
   Fig_labels <-  sort(Fig_labels, decreasing = FALSE)
-  TPM_select$plant <- factor(TPM_select$plant, levels=Fig_labels)
-Logfold_plot <- ggplot(TPM_select, aes(x = log2, y = plant, fill=signif, size=TPM_abund)) +
+  MRPM_select$plant <- factor(MRPM_select$plant, levels=Fig_labels)
+Logfold_plot <- ggplot(MRPM_select, aes(x = log2, y = plant, fill=signif, size=MRPM_abund)) +
   geom_point(alpha=0.9, shape=21, color="black") +   
   scale_fill_manual(values = c("No"="white" , "Yes"="#009900")) +
   facet_wrap(vars(SS), scales = "free_x", ncol=11) +
